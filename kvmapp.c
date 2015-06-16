c4921244968919e0b63e45863a9d80db44d6e56a
Sergey Lungu <sergey.lungu@gmail.com>
2015-06-17 00:44:41 +0300

    Added basic KVM_SET_USER_MEMORY_REGION interface.

--- kvmapp.c
+++ kvmapp.c
@@ -12,6 +12,7 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/user.h>
 #include <unistd.h>
 
 #include <linux/kvm.h>
@@ -29,12 +30,14 @@ struct vm {
 	int vcpu_mmap_size;              /**< size of shared vCPU region */
 	int vcpu_fd[MAX_VCPUS];          /**< vCPU file descriptors */
 	struct kvm_run *vcpu[MAX_VCPUS]; /**< vCPU kvm_run structures */
+	unsigned num_mem_slots;          /**< number of attached mem slots */
 };
 
 static int  kvm_open(const char *);
 
 static int  vm_create(int, struct vm *);
 static int  vm_create_vcpu(struct vm *);
+static int  vm_attach_memory(struct vm *, uintptr_t, size_t, void *);
 static void vm_destroy(struct vm *);
 
 /**
@@ -101,6 +104,7 @@ int vm_create_vcpu(struct vm *vm)
 	int i;
 
 	assert(vm != NULL);
+	assert(vm->vm_fd > 0);
 
 	i = vm->num_vcpus;
 	if (i < MAX_VCPUS) {
@@ -119,6 +123,37 @@ int vm_create_vcpu(struct vm *vm)
 	}
 
 	return ret;
+}
+
+/**
+ * Attach a memory region to a virtual machine
+ *
+ * @param vm   virtual machine descriptor
+ * @param gpa  guest physical address
+ * @param size memory region size
+ * @param addr start of host addressable memory region
+ *
+ * @return ID of created memory region, or -1 if an error occured
+ */
+static int vm_attach_memory(struct vm *vm, uintptr_t gpa, size_t size, void *addr)
+{
+	struct kvm_userspace_memory_region mem;
+
+	assert(vm != NULL);
+	assert(vm->vm_fd > 0);
+	assert(size > 0 && size % PAGE_SIZE == 0);
+	assert(addr != NULL);
+
+	mem.slot = vm->num_mem_slots;
+	mem.flags = 0; /* read/write */
+	mem.guest_phys_addr = gpa;
+	mem.memory_size = size;
+	mem.userspace_addr = (uintptr_t) addr;
+
+	if (ioctl(vm->vm_fd, KVM_SET_USER_MEMORY_REGION, &mem) == 0)
+		return vm->num_mem_slots++;
+
+	return -1;
 }
 
 /**
@@ -145,12 +180,42 @@ void vm_destroy(struct vm *vm)
 
 int main(int argc, const char *argv[])
 {
-	int kvm = kvm_open(KVM_PATH);
+	static const size_t num_bytes = 0x100000;
+
+	int kvm;
 	struct vm vm;
-	assert(vm_create(kvm, &vm) == 0);
-	assert(vm_create_vcpu(&vm) == 0);
-	assert(vm_create_vcpu(&vm) == 1);
+	void *guestmem;
+	int ret = EXIT_FAILURE;
+
+	kvm = kvm_open(KVM_PATH);
+	if (kvm < 0)
+		goto out;
+
+	if (vm_create(kvm, &vm) != 0)
+		goto out_kvm;
+
+	if (vm_create_vcpu(&vm) < 0)
+		goto out_vm;
+
+	guestmem = mmap(0, num_bytes, PROT_READ | PROT_WRITE,
+			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (guestmem == MAP_FAILED)
+		goto out_vm;
+
+	if (vm_attach_memory(&vm, 0x0, num_bytes, guestmem) < 0)
+		goto out_guestmem;
+
+	ret = EXIT_SUCCESS;
+
+out_guestmem:
+	munmap(guestmem, num_bytes);
+
+out_vm:
 	vm_destroy(&vm);
+
+out_kvm:
 	close(kvm);
-	return EXIT_SUCCESS;
+
+out:
+	return ret;
 }
