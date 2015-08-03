358f8fa0fe5e398a9761af1655fb86c2662ba736
Sergey Lungu <sergey.lungu@gmail.com>
2015-08-04 01:12:54 +0300

    Cleaned up top-level virtual machine initialization code.

--- kvmapp.c
+++ kvmapp.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,9 +10,24 @@
 #include "kvm.h"
 #include "loader/binary.h"
 #include "log.h"
+#include "vcpu.h"
 
-#define KVM_PATH  "/dev/kvm" /* default path to KVM subsystem device file */
-#define NUM_BYTES 0x100000   /* default guest memory size                 */
+#define DEFAULT_KVM_PATH   "/dev/kvm" /* default path to KVM device file */
+#define DEFAULT_IMAGE_PATH NULL       /* default guest image file path   */
+#define DEFAULT_NUM_BYTES  0x100000   /* default guest memory size       */
+
+/**
+ * struct config - parsed command line arguments
+ *
+ * @kvm_path:   path to KVM subsystem device file
+ * @image_path: guest image file path
+ * @num_bytes:  guest memory size in bytes
+ */
+struct config {
+	const char *kvm_path;
+	const char *image_path;
+	size_t num_bytes;
+};
 
 /**
  * uasge() - print usage information to supplied output stream and exit
@@ -20,6 +36,9 @@
  */
 static void NORETURN usage(const char *progname, FILE *stream)
 {
+	assert(progname != NULL);
+	assert(stream != NULL);
+
 	fprintf(stream, "Usage: %s [-h] [-k KVM_PATH] [-m MEGABYTES] IMAGE\n",
 		progname);
 
@@ -28,102 +47,41 @@ static void NORETURN usage(const char *progname, FILE *stream)
 }
 
 /**
- * start_vm() - initialize and start a virtual machine
+ * parse_command_line() - parse command line arguments and return
+ *                        configuration structure
  *
- * @kvm_path:   path to KVM subsystem device file
- * @image_path: path to guest image file
- * @num_bytes:  size of guest memory in bytes
+ * @argc: number of command line arguments
+ * @argv: list of command line arguments
  *
- * Return: exit code value
+ * Return: pointer to a parsed configuration structure
  */
-static int start_vm(const char *kvm_path, const char *image_path,
-		    size_t num_bytes)
+static const struct config *parse_command_line(int argc, char *argv[])
 {
-	int rc, kvm;
-	struct vm *vm;
-	void *guestmem;
-	struct kvm_run *vcpu;
-	int ret = EXIT_FAILURE;
-
-	kvm = kvm_open(kvm_path);
-	if (kvm < 0)
-		goto out;
-
-	vm = vm_create(kvm);
-	if (vm == NULL)
-		goto out_kvm;
-
-	if (vcpu_create(vm) < 0)
-		goto out_vm;
-
-	guestmem = mmap(0, num_bytes, PROT_READ | PROT_WRITE,
-			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if (guestmem == MAP_FAILED)
-		goto out_vm;
-
-	if (vm_attach_memory(vm, 0x0, num_bytes, guestmem) < 0)
-		goto out_guestmem;
-
-	rc = binary_load(vm, image_path, 0,
-			 BINARY_LOAD_PROTECTED | BINARY_LOAD_PAGED);
-	if (rc != 0)
-		goto out_guestmem;
-
-	vcpu = vcpu_get(vm, 0);
-	for (/* NOTHING */; /* NOTHING */; /* NOTHING */) {
-		rc = vcpu_run(vm, 0);
-		if (rc != 0)
-			break;
-
-		if (vcpu->exit_reason == KVM_EXIT_HLT)
-			break;
-
-		if (vcpu->exit_reason == KVM_EXIT_IO &&
-		    vcpu->io.port == 0x3f8 &&
-		    vcpu->io.direction == KVM_EXIT_IO_OUT)
-		{
-			write(STDOUT_FILENO,
-			      (const void *) vcpu + vcpu->io.data_offset,
-			      vcpu->io.size * vcpu->io.count);
-		}
-	}
-
-	ret = EXIT_SUCCESS;
-
-out_guestmem:
-	munmap(guestmem, num_bytes);
-
-out_vm:
-	vm_destroy(vm);
-
-out_kvm:
-	kvm_close(kvm);
-
-out:
-	return ret;
-}
-
-int main(int argc, char *argv[])
-{
-	const char *kvm_path = KVM_PATH;
-	const char *image_path = NULL;
-	size_t num_bytes = NUM_BYTES;
 	char *num_bytes_endptr;
 	int opt;
+
+	static struct config cfg = {
+		.kvm_path   = DEFAULT_KVM_PATH,
+		.image_path = DEFAULT_IMAGE_PATH,
+		.num_bytes  = DEFAULT_NUM_BYTES
+	};
+
+	assert(argc > 0);
+	assert(argv != NULL);
 
 	while ((opt = getopt(argc, argv, "k:m:h")) != -1)
 		switch (opt) {
 		case 'k':
-			kvm_path = optarg;
+			cfg.kvm_path = optarg;
 			break;
 		case 'm':
-			num_bytes = strtol(optarg, &num_bytes_endptr, 10);
+			cfg.num_bytes = strtol(optarg, &num_bytes_endptr, 10);
 			if (*num_bytes_endptr != '\0') {
 				errorx("%s: wrong guest memory size", optarg);
 				usage(argv[0], stderr);
 				/* NOTREACHED */
 			}
-			num_bytes <<= 20;
+			cfg.num_bytes <<= 20;
 			break;
 		case 'h':
 			/* FALLTHROUGH */
@@ -138,7 +96,116 @@ int main(int argc, char *argv[])
 		/* NOTREACHED */
 	}
 
-	image_path = argv[optind];
+	cfg.image_path = argv[optind];
 
-	return start_vm(kvm_path, image_path, num_bytes);
+	return &cfg;
+}
+
+/**
+ * create_virtual_machine() - create a virtual machine
+ *
+ * @cfg:      parsed command line arguments
+ * @kvm:      KVM subsystem descriptor
+ * @guestmem: allocated guest memory of size cfg->num_bytes
+ *
+ * Return: virtual machine descriptor, or NULL if an error occurred
+ */
+static struct vm *create_virtual_machine(const struct config *cfg,
+					 int kvm, void *guestmem)
+{
+	struct vm *vm;
+
+	assert(cfg != NULL);
+	assert(kvm > 0);
+	assert(guestmem != NULL);
+
+	vm = vm_create(kvm);
+	if (vm == NULL)
+		return NULL;
+
+	if (vcpu_create(vm) < 0)
+		goto err;
+
+	if (vm_attach_memory(vm, 0x0, cfg->num_bytes, guestmem) < 0)
+		goto err;
+
+	if (binary_load(vm, cfg->image_path, 0,
+			BINARY_LOAD_PROTECTED | BINARY_LOAD_PAGED) != 0)
+		goto err;
+
+	return vm;
+
+err:
+	vm_destroy(vm);
+	return NULL;
+}
+
+/**
+ * run_virtual_machine() - start a run loop for a virtual machine
+ *
+ * @vm: virtual machine to run
+ *
+ * Return: zero on clean virtual machine exit, or a non-zero value on error
+ */
+static int run_virtual_machine(struct vm *vm)
+{
+	struct kvm_run *vcpu;
+
+	assert(vm != NULL);
+
+	vcpu = vcpu_get(vm, BOOT_VCPU);
+	for (/* NOTHING */; /* NOTHING */; /* NOTHING */) {
+		if (vcpu_run(vm, 0) != 0)
+			return EXIT_FAILURE;
+
+		if (vcpu->exit_reason == KVM_EXIT_HLT)
+			return EXIT_SUCCESS;
+
+		if (vcpu->exit_reason == KVM_EXIT_IO &&
+		    vcpu->io.port == 0x3f8 &&
+		    vcpu->io.direction == KVM_EXIT_IO_OUT)
+		{
+			write(STDOUT_FILENO,
+			      (const void *) vcpu + vcpu->io.data_offset,
+			      vcpu->io.size * vcpu->io.count);
+		}
+	}
+
+	/* NOTREACHED */
+	return EXIT_FAILURE;
+}
+
+int main(int argc, char *argv[])
+{
+	const struct config *cfg;
+	int ret = EXIT_FAILURE;
+	void *guestmem;
+	struct vm *vm;
+	int kvm;
+
+	cfg = parse_command_line(argc, argv);
+	assert(cfg != NULL);
+
+	kvm = kvm_open(cfg->kvm_path);
+	if (kvm < 0)
+		return EXIT_FAILURE;
+
+	guestmem = mmap(0, cfg->num_bytes, PROT_READ | PROT_WRITE,
+			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (guestmem == MAP_FAILED) {
+		kvm_close(kvm);
+		fail("mmap");
+		/* NOTREACHED */
+	}
+
+	vm = create_virtual_machine(cfg, kvm, guestmem);
+	if (vm != NULL) {
+		ret = run_virtual_machine(vm);
+		vm_destroy(vm);
+	}
+
+	munmap(guestmem, cfg->num_bytes);
+	kvm_close(kvm);
+
+	return ret;
 }
